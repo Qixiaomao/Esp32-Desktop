b259f4700892cfab6efb98d1be7a1018e49b56a1
Seven <Qixiaomao@gmail.com>
2023-07-30 13:14:38 +0800

    push v4.0

---
+++ main/include/data/ds_data_page.h
@@ -0,0 +1,11 @@
+
+#ifndef _DS_DATA_PAGE_H_
+#define _DS_DATA_PAGE_H_
+
+extern const unsigned char gImage_main_page_wifi[];
+
+extern const unsigned char gImage_main_page[];
+
+extern const unsigned char gImage_setting_page[];
+
+#endif
